8b5170d8e5868950af9ad04e3966bcb291f9a76b
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-12-01 00:42:43 +0000

    Modified comment

--- Windows Terminal/run_nslookup.cmd
+++ Windows Terminal/run_nslookup.cmd
@@ -1,5 +1,5 @@
-set /p website=Enter the website you want to query:
+set /p website=Enter the website you want to query: 
 nslookup %website%
 pause 
 REM this query will return the IP address of the domain name
-REM you can replace google.com with any domain name you want to query
+REM set /p takes a user input and assigns it to the variable website
