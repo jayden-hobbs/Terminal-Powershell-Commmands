e330437fe9c7086288a1948c6e064e94b61a5bab
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-12-01 00:38:37 +0000

    Changed :: to REM in all codes

--- Windows Terminal/file_handling.cmd
+++ Windows Terminal/file_handling.cmd
@@ -1,10 +1,10 @@
-cd :: this moves the current directory to what is specified
-dir :: this lists the contents of the current directory
-mkdir :: this creates a new folder within the current directory
-rmdir :: this removes a folder within the current directory (if it is empty)
-del :: this deletes a file within the current directory
-copy :: this copies a file from one location to another
-move :: this moves a file from one location to another
-ren :: this renames a file
-xcopy :: this copies a file from one location to another, and can copy entire directories
-attrib :: displays or changes file attributes
+cd REM this moves the current directory to what is specified
+dir REM this lists the contents of the current directory
+mkdir REM this creates a new folder within the current directory
+rmdir REM this removes a folder within the current directory (if it is empty)
+del REM this deletes a file within the current directory
+copy REM this copies a file from one location to another
+move REM this moves a file from one location to another
+ren REM this renames a file
+xcopy REM this copies a file from one location to another, and can copy entire directories
+attrib REM displays or changes file attributes
--- Windows Terminal/run_systemINFO.cmd
+++ Windows Terminal/run_systemINFO.cmd
@@ -1,3 +1,3 @@
 systeminfo
 pause
-:: Displays info such as the OS name, version, build, and manufacturer
+REM Displays info such as the OS name, version, build, and manufacturer
