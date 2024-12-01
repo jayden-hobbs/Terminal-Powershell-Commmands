6f3fcc46fa6b6e4e85e363f1ae2d60bd2c1ae3cf
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-12-01 00:41:15 +0000

    Added user input

--- Windows Terminal/run_nslookup.cmd
+++ Windows Terminal/run_nslookup.cmd
@@ -1,4 +1,5 @@
-nslookup google.com
+set /p website=Enter the website you want to query:
+nslookup %website%
 pause 
 REM this query will return the IP address of the domain name
 REM you can replace google.com with any domain name you want to query
