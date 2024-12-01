466dca6af625a144a7fb7853517d061844571bfc
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-12-01 00:18:23 +0000

    Added pause functions

--- Windows Terminal/run_ipconfig.cmd
+++ Windows Terminal/run_ipconfig.cmd
@@ -1,4 +1,4 @@
 ipconfig
-
+pause
 :: This shows info such as IPv4 address, IPv6 address, subnet mask, and default gateway of the current device
 
--- Windows Terminal/run_systemINFO.cmd
+++ Windows Terminal/run_systemINFO.cmd
@@ -1,3 +1,3 @@
 systeminfo
-
+pause
 :: Displays info such as the OS name, version, build, and manufacturer
